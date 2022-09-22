e17485fa60055f3372eee66bb20fb2dd96420c6d
moses ngige <moses32ngigz@gmail.com>
2022-09-22 17:01:16 +0300

    commit tree 225832d46e6efaa3fc20278004c0d6b911fbdd7c

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,12 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  
+  # def self.by_name(name)
+  #   Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+  # end
+  def self.by_name(name)
+    Student.where(first_name:"#{name[0]}")
+  end
+
 end
